e6a4ed8ccb08d3373b8aa8b3e6772fe54f56f2e9
EdwinYHL <126517767+EdwinYHL@users.noreply.github.com>
2023-05-02 19:52:03 +0000

    deleted:    Practice 	new file:   Practice/Practice.csproj 	new file:   Practice/Program.cs

---
+++ Practice/Program.cs
@@ -0,0 +1,28 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+         int? edad=null;
+        Console.WriteLine("Captura tu edad y presiona ¨Enter¨ ");
+        string? edadCapturada = Console.ReadLine();
+        if (edadCapturada is not null)
+        {
+            try 
+            {
+                edad = int.Parse(edadCapturada);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Ingrese un valor Numerico");
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Esa es una edad muy grande o muy pequeña, ingrese otra.");
+            }
+        } 
+        if (edad >0 && edad is not null)
+        Console.WriteLine($"Tu edad es {edad}");
+        else
+        Console.WriteLine($"No se capturo una edad Valida");
+    }
+}
